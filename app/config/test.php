d309dc529ace8d2770ba0fb0777307517b7fccb1
ilvval <ilv-val@mail.ru>
2014-01-16 16:33:48 +0400

    test

--- app/config/test.php
+++ app/config/test.php
@@ -9,6 +9,7 @@
  * @copyright 2013 2amigOS! Consultation Group LLC
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
+ // test
 return array(
 	'params' => array(
 		'yii.handleErrors'   => false,
